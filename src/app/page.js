6e733152aef016c0fed841775c3251e9fb5a6c4e
tanvirtbf <tanver018765@gmail.com>
2024-06-11 10:52:25 +0600

    Home page button complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>Hello World!</main>
+    <main className="raffle-btn">
+      <Link href={"/raffle-draw"}>Raffle Draw Page</Link>
+    </main>
   );
 }
