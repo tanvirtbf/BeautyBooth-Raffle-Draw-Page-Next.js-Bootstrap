81a9cfdd4bfe03bcde9bdb378c001c205213550c
tanvirtbf <tanver018765@gmail.com>
2024-06-11 11:06:52 +0600

    global css huge change

--- src/app/raffle-draw/layout.jsx
+++ src/app/raffle-draw/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={roboto.className} style={{backgroundColor: '#FFFFFF'}}>
         {children}
       </body>
     </html>
--- src/app/raffle-draw/page.jsx
+++ src/app/raffle-draw/page.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './raffle-draw.module.css'
+import Image from 'next/image'
+import Banner from '../../../public/image 40.png'
 
 const RaffleDraw = () => {
   return (
     <div className='container'>
-      
+      <div className='row'>
+        <div className={`${styles.mainBannerImg} col`} >
+          <Image src={Banner}/>
+        </div>
+      </div>
     </div>
   )
 }
