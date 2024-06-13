607837ca03f1f316f2383d43b28c2c73b18f1491
tanvirtbf <tanver018765@gmail.com>
2024-06-13 11:27:27 +0600

    badge complete

--- src/app/raffle-draw/page.jsx
+++ src/app/raffle-draw/page.jsx
@@ -64,8 +64,8 @@ const RaffleDraw = () => {
                   title={item.title}
                   person={item.person}
                   url={item.img}
-                  isBadge={i<=3 ? true : false }
-                  prizeNo={i<=3 ? i : null}
+                  isBadge={i<3 ? true : false }
+                  prizeNo={i<3 ? i+1 : null}
                 />
               </div>
             ))}
